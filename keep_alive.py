5bc0d658f95ff549b39d8bdf0f442321f7165427
dogannakliyat <ravenisthe@gmail.com>
2026-08-26 12:19:25 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "DSP Bot Aktif ve Çalışıyor!"
+    return "DSP Bot Aktif ve Çalışıyor!", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
