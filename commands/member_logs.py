693cd2dafaad4c04a34696b9a7e3cdb4fc5d8e93
dogannakliyat <ravenisthe@gmail.com>
2026-08-26 13:21:33 +0300

    Update member_logs.py

--- commands/member_logs.py
+++ commands/member_logs.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import config
 
 LOG_CHANNEL_ID = 1537161700380377138
+AUTOPING_CHANNEL_ID = 1537158034294448128
 
 class MemberLogs(commands.Cog):
     def __init__(self, bot):
@@ -21,25 +22,38 @@ class MemberLogs(commands.Cog):
                 except Exception:
                     pass
 
-        # 2. Giriş Mesajını Gönderme
-        channel = member.guild.get_channel(LOG_CHANNEL_ID)
-        if channel:
+        # 2. Giriş Log Mesajını Gönderme (Kalıcı Log)
+        log_channel = member.guild.get_channel(LOG_CHANNEL_ID)
+        if log_channel:
             total_members = member.guild.member_count
             msg = f"<a:erensigiris:1537179743445712926> {member.mention} Katıldı! {total_members}"
             try:
-                await channel.send(msg)
+                await log_channel.send(msg)
+            except Exception:
+                pass
+
+        # 3. Auto-Ping Mesajı (10 Saniye Sonra Otomatik Silinir)
+        ping_channel = member.guild.get_channel(AUTOPING_CHANNEL_ID)
+        if ping_channel:
+            welcome_text = (
+                f"*👋🏻 {member.mention} Hoş Geldiniz, "
+                f"Bu Formu Kullanarak https://discord.com/channels/1537126439739199619/1537157370264944690 "
+                f"Bu Kanaldan Kayıt Olabilirsiniz.*"
+            )
+            try:
+                await ping_channel.send(welcome_text, delete_after=10)
             except Exception:
                 pass
 
     # Sunucudan Biri Ayrıldığında
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        channel = member.guild.get_channel(LOG_CHANNEL_ID)
-        if channel:
+        log_channel = member.guild.get_channel(LOG_CHANNEL_ID)
+        if log_channel:
             total_members = member.guild.member_count
             msg = f"<a:erensicikis:1537179768641028168> {member.mention} Ayrıldı! {total_members}"
             try:
-                await channel.send(msg)
+                await log_channel.send(msg)
             except Exception:
                 pass
 
